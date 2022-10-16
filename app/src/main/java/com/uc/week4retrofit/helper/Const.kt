0c9d6d6c562a15e7d2352717cb3ec0b1950df1d9
dwarddx <edwardhartantio27@gmail.com>
2022-10-16 22:38:15 +0700

    Week 4 - 0706012110012

---
+++ app/src/main/java/com/uc/week4retrofit/helper/Const.kt
@@ -0,0 +1,7 @@
+package com.uc.week4retrofit.helper
+
+object Const {
+    const val BASE_URL = "https://api.themoviedb.org/3/"
+    const val API_KEY = "d77b4a595b5a4a8c9eacb7d5bae5ccfa"
+    const val IMG_URL = "https://image.tmdb.org/t/p/w500/"
+}
